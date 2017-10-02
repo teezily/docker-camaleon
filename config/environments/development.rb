0657a39577722ec358f81446f93c5f716f87398b
kinyaao <kouassi.ulric@gmail.com>
2017-10-02 18:06:25 +0200

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  default_host = ENV['DOCKER_HOST_PORT'] || 'localhost:3000'
+  default_url_options[:host] = default_host
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -11,20 +14,6 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
-  # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -46,7 +35,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   #Assets host url to get assets in an other app
-  config.action_controller.asset_host = "#{ENV['CAMALEON_HOST']}"
+  config.action_controller.asset_host = "#{ENV['DOCKER_HOST_PORT']}"
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
@@ -54,7 +43,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  default_host = ENV['DOCKER_HOST_PORT'] || 'localhost:3000'
-  default_url_options[:host] = default_host
 end
